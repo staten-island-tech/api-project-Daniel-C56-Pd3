f8f89e2037b852890b8ae2a04b2a3556fad2feec
Daniel <danield494@nycstudents.net>
2026-01-12 13:19:34 -0500

    1

--- api/src/index.js
+++ api/src/index.js
@@ -1,37 +1,3 @@
-/*async function getData() {
-  try {
-    const response = await fetch(
-      "https://www.themealdb.com/api/json/v1/1/filter.php?a=Chinese"
-    );
-
-    if (!response.ok) {
-      throw new Error("Failed to fetch meals");
-    }
-
-    const data = await response.json();
-document.getElementById("meals").innerHTML = data.meals
-  .map(
-    (meal) => `
-    <div class="w-100 p-2 rounded-lg shadow-sm bg-white flex flex-col items-center gap-1">
-      <img 
-        src="${meal.strMealThumb}" 
-        alt="${meal.strMeal}" 
-        alt="${meal.idmeal}"
-        class="w-100 h-100 object-cover rounded-md"
-      />
-      <p class="text-xs text-center font-medium leading-tight">
-        ${meal.strMeal}
-      </p>
-    </div>
-  `
-  )
-  .join("");
-  } catch (error) {
-    console.error(error);
-  }
-}
-getData();*/
-
 async function getMealsByArea(area) {
   try {
     const response = await fetch(
@@ -70,13 +36,13 @@ function displayMeals(meals) {
   document.getElementById("meals").innerHTML = meals
     .map(
       (meal) => `
-      <div class="bg-white p-2 rounded shadow flex flex-col items-center">
+      <div class="box-content size-200 border-4 p-4">
         <img
           src="${meal.strMealThumb}"
           alt="${meal.strMeal}"
-          class="w-full h-32 object-cover rounded"
+          class="w-200 h-200 object-cover rounded-md"
         />
-        <p class="text-xs text-center mt-1 font-medium">
+        <p class="text-xs text-center font-medium leading-tight">
           ${meal.strMeal}
         </p>
       </div>
