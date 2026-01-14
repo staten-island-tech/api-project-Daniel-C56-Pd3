9380c6e452dc67a0d0fe78083449f78b0b53e6e4
Daniel <danield494@nycstudents.net>
2026-01-14 13:16:08 -0500

    Update practice.js

--- practice.js
+++ practice.js
@@ -151,31 +151,9 @@ const videoGames = [
   },
 ];
 
-const publishers = { nintendo: 1};
 
-videoGames.forEach((game)=>{
-  let studio = game.studio;
-  if(!publishers[studio]){
-    publishers[studio] = 1;
-  }else {
-    publishers[studio] = publishers[studio] + 1;
-  }
-});
-console.log(publishers);
-for (let publisher in publishers){
-  let publishDiv = document.querySelector(".publishers");
-  publishDiv.insertAdjacentHTML(
-    "afterbegin", 
-    `
-    <div class = "publisher-container">
-    <h2 class = "publisher">${publisher}</h2>
-    <h3 class = "amount">${publishers[publisher]}</h3>
-    </div>
-    `);
-}
-
-videoGames.filter((game) => game.ratings.metacritic > 95)
-.forEach((game)=> document
-.querySelector("games")
-.insertAdjacentHTML("afterbegin", `<h2>${game.title}</h2>`)
-);
+let count = 0
+	videoGames.foreach(game => {
+if (game.platform.includes("Nintendo Switch"));
+count++;});
+	console.log(count)
